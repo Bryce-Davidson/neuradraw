8948c8b4aa1a453d791fa8040e3e1da8a5781724
Bryce <bryce_davidson@icloud.com>
2020-08-02 18:53:44 -0500

    added a new layer drawing procedure

--- Structures/Deep_Neural_Network.js
+++ Structures/Deep_Neural_Network.js
@@ -1,31 +1,114 @@
+// new p5();
+
 class DeepNeuralNetwork {
     constructor() {
         this.layers = [];
         this.colors = [];
     }
 
-    add_layers(layers) {
-        this.layers = layers;
-    }
-
-    add_colors(colors) {
-        this.colors = colors;
-    }
-
-    add_layer(size, color) {
-        let new_layer = new DeepNeuralNetworkLayer(size, color);
+    /**
+    * Adds a layer to the fully connected deep neural network
+    * 
+    * @param{Number} size - the size of the layer
+    * @param{String} color - the color of the layer 
+    * @param{String} name - the name of the layer
+    * @returns nothing
+    */
+    add_layer(size, color, name) {
+        // Do i need this here or should i make it so that the layers are added exlipcit
+        let new_layer = new DeepNeuralNetworkLayer(size, color, name);
         this.layers.push(new_layer);
     }
 
-    add_annotation() {
+    /**
+    * Adds an annotation config object to the specified layer
+    * 
+    * @param{Number} layer_index - the index of the layer to recieve annotations
+    * @param{Object} annotations - the annotation config object
+    * @example
+    *   nn.add_annotations(0, {
+    *       layer: {
+    *           above: {
+    *               dimensions: [20, 40], 
+    *               type: "latex",
+    *               text: "\\int{x^2}"
+    *           }
+    *       } 
+    *   })
+    * 
+    * @returns nothing
+    */
+    add_annotations(layer_index, annotations) {
+        if(layer_index == undefined)
+            throw new Error("Need to include a layer index to add annotations")
 
+        // console.log(layer_index, annotations);
+        var layer = this.layers[layer_index];
+        layer.add_annotations(annotations);
     }
 
+
+    /**
+    * Draw a neural network from annotations example
+    * 
+    * @param{Number} x - the x position of the graph (from left)
+    * @param{Number} y - the y position of the graph (from top)
+    * @param{Number} radius - the radii of the nodes in the graph
+    * @param{Number} layer_spacing - the spacing between each layer in the graph
+    * @param{Number} node_spacing - the spacing between each node in a layer
+    */
+    draw(x, y, radius, layer_spacing, node_spacing) {
+
+        let vertical_spacing = radius + node_spacing
+        let horizontal_spacing = radius + layer_spacing
+
+        var sizes = [];
+        for(var i=0; i < this.layers.length; i++)
+            sizes.push(this.layers[i].size);
+        
+        // TODO:
+            // Need to implement the randomize weights function
+
+        // draw the edges for the particular layer
+        this._draw_edges(x, y, sizes, vertical_spacing, horizontal_spacing, radius);
+
+        for(var i=0; i < this.layers.length; i++) {
+            // get the layer from the layers array
+            var layer = this.layers[i];
+            // console.log(layer);
+            let layer_top = vertical_spacing*(layer.size+max(sizes))/2 + y;
+
+            var annotations = layer.annotations || NaN;
+            // console.log(annotations);
+            // For each node
+            for(var j=0; j < layer.size; j++) {
+                let center_x = x + 1+radius/2 + i*horizontal_spacing;
+                let center_y = layer_top - j*vertical_spacing;
+                
+                // Push a new drawing stack
+                push();
+                fill(layer.color || "white")
+                stroke(layer.color || "black")
+                circle(center_x, center_y, radius)
+                pop();
+            }
+        }
+    }
+
+
+    /**
+    * Quickly draw a fully connected neural network.
+    * 
+    * @param{Number} x - the x position of the graph (from left)
+    * @param{Number} y - the y position of the graph (from top)
+    * @param{[Number]} layers - an array containing the layer sizes
+    * @param{[String]} colors - an array containging the the layer colors
+    */
     quick_draw(x, y, layers, colors, radius, layer_spacing, node_spacing) {
         var vertical_spacing = radius + node_spacing
         var horizontal_spacing = radius + layer_spacing
 
-        this.draw_edges(x, y, layers, vertical_spacing, horizontal_spacing, radius);
+        this._draw_edges(x, y, layers, vertical_spacing, horizontal_spacing, radius);
         
         for(var i=0; i < layers.length; i++) {
             var layer_size = layers[i];
@@ -41,7 +124,7 @@ class DeepNeuralNetwork {
         }
     }
 
-    draw_edges(x, y, layers, vertical_spacing, horizontal_spacing, radius) {
+    _draw_edges(x, y, layers, vertical_spacing, horizontal_spacing, radius) {
         // Gather layers sizes
         var a = layers.slice(0,-1);
         var b = layers.slice(1);
@@ -73,16 +156,20 @@ class DeepNeuralNetwork {
 }
 
 class DeepNeuralNetworkLayer {
-    constructor(size, color) {
+    constructor(size, color, name) {
         this.size = size;
         this.color = color;
-        this.above = "";
-        this.below = "";
-        this.input = "";
-        this.output = "";
+        this.annotations = {};
     }
-}
 
-function randInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    /**
+     * @param {Object} annotations
+     */
+    add_annotations(annotations) {
+        this.annotations = annotations;
+    }
+
+    // Maybe draws the layer upon calling. 
+    // Don't know what i need yet for the layer to be drawn correctly
+    draw() {}
 }
--- sketch.js
+++ sketch.js
@@ -1,9 +1,30 @@
+// new p5();
 var nn =  new DeepNeuralNetwork();
 
+nn.add_layer(2, "green", "input")
+nn.add_layer(3, "blue", "h_1")
+nn.add_layer(2, "red", "output")
+
+console.log(nn);
+nn.add_annotations()
+
+console.log(nn);
+
 function setup() {
-    createCanvas(900, 1000);
+    createCanvas(1300, 1300);
     noLoop();
-    var layers = [2,3,3,3,1]
+    
+    // nn.draw(x=90, y=30, radius=60, layer_spacing=100, node_spacing=40)
+
+
+    // var col = color(255,0,244);
+    // tex = createP();
+    // tex = createElement
+    // tex.style('font-size', '20px');
+    // tex.position(135, 165);
+    // katex.render('', tex.elt);
+
+    var layers = [2,4,5,5,5,3,2]
     // var colors = ["green", "blue", "blue", "red"]
     nn.quick_draw(x=90, y=30, layers, 0, radius=60, layer_spacing=100, node_spacing=40);
 }
