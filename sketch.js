41954aa31a49e57bafbe45414640dffaf9cd4664
Bryce <bryce_davidson@icloud.com>
2020-09-03 17:03:37 -0700

    Working changing github to merge into one repo

--- sketch.js
+++ sketch.js
@@ -9,22 +9,27 @@ input_annotations = {
     }
 }
 
-nn.add_layer(1, "green", "input", input_annotations)
-nn.add_layer(2, "blue", "h_1", input_annotations)
+nn.add_layer(2, "green", "input", input_annotations)
+nn.add_layer(3, "blue", "h_1", input_annotations)
+nn.add_layer(6, "blue", "h_2", input_annotations)
+nn.add_layer(7, "blue", "h_3", input_annotations)
+nn.add_layer(3, "red", "output", input_annotations)
 
 
 
 function setup() {
     createCanvas(2000, 2000);
     circle(100,100,100)
-    noLoop();
+    frameRate(30);
+    // noLoop();
 }
 
 function draw() {
     clear();
-
+    // console.log(frameCount);
     nn.draw({
-        node_spacing: 60,
+        node_spacing: 60 + 60*Math.sin(frameCount/100),
+        layer_spacing: Math.abs(300*Math.sin(frameCount/100)),
         weight_thicknesses: 1
     });
 }
